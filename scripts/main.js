b0a6a9377b960b31a1e29c6221090d05bc915c3c
Kenny Lee <kenny.lee927@gmail.com>
2022-04-26 21:18:03 -0400

    implement basic modules and factory function for game logic

--- scripts/main.js
+++ scripts/main.js
@@ -14,8 +14,6 @@
 // console.log(jeff.name);
 // jeff.sayHello();
 
-
-
 // MODULE FUNCTION WHEN ONLY ONE
 
 // THIS IS A FACTORY
@@ -28,7 +26,6 @@
 // const calc1 = calculator(3, 5);
 // calc1.add();
 
-
 // THIS IS A MODULE (wrapped in IIFE and immediately called)
 // const calculator = (() => {
 //     const add = (a, b) => a + b;
@@ -36,28 +33,54 @@
 //       add
 //     };
 //   })();
-  
-//   calculator.add(3,5); // 8
 
-const gameBoard = (() => {
-    const startBoard = [[1, 2, 3], [4,5,6], [7,8,9]];
+//   calculator.add(3,5); // 8
+const game = (() => {
+    let turn = true;
     return {
-        startBoard
+        turn,
     };
 })();
 
-const playerFactory = function(name, sign) {
+//gameboard MODULE
+const gameBoard = (() => {
+    const startBoard = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+    ];
+    const cells = document.querySelectorAll(".cell");
+    cells.forEach((cell) => {
+        cell.addEventListener("click", () => {
+            if (game.turn == true && cell.textContent=="") {
+            cell.textContent = "X";
+            game.turn = false;
+            }
+            else if (game.turn == false && cell.textContent=="") {
+            cell.textContent = "O";
+            game.turn = true;
+            }
+        });
+         
+    });
+    return {
+        startBoard,
+    };
+})();
+
+//player FACTORY
+const playerFactory = function (name, sign) {
     const sayHello = function () {
         console.log("hello!");
-    }
-
-    return {name, sign, sayHello};
+    };
+    return { name, sign, sayHello };
 };
 
-const jeff = playerFactory ("jeff", "x");
+const jeff = playerFactory("jeff", "x");
 console.log(jeff.name);
 jeff.sayHello();
 
+// game object
 
 // gameBoard = module
 
