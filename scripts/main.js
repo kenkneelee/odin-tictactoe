9d2d2c4203e689d78f83ec3cf81d9743e1054a93
Kenny Lee <kenny.lee927@gmail.com>
2022-04-28 17:26:00 -0400

    start writing check win function for game module

--- scripts/main.js
+++ scripts/main.js
@@ -36,18 +36,53 @@
 
 //   calculator.add(3,5); // 8
 const game = (() => {
+    // keep track of who's turn it is
     let turn = true;
     const headertest = document.querySelector("header");
-    const switchTurn = function() {
+    const switchTurn = function () {
         if (this.turn == true) {
             this.turn = false;
             headertest.textContent = "Player 1's turn";
-        }
-        else {
+        } else {
             this.turn = true;
             headertest.textContent = "Player 2's turn";
         }
-    }
+    };
+
+    // after every move, check for a winner
+    const winningCombos = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+        [1, 4, 7],
+        [2, 5, 8],
+        [3, 6, 9],
+        [1, 5, 9],
+        [3, 5, 7],
+    ];
+    // const player1total = array of all of player 1's ticks
+    // const player2total = array of all of player 2's ticks
+
+    const checkWin = function () {
+        // for every winning combo,
+        for (let i = 0; i < winningCombos.length(); i++) {
+            // for every cell in the winning combo
+                const winner = winningCombos[i];
+                let checker = (winner, playerTotal) => winner.every(v => winner.includes(v));
+                // let array1 = [1,2,3],
+                // array2 = [1,2,3,4],
+                // array3 = [1,2];
+                // let checker = (arr, target) => target.every(v => arr.includes(v));
+                // console.log(checker(array2, array1));  // true
+                // console.log(checker(array3, array1));  // false
+
+            // const checkWin = function {
+            //     if player1total.contains()
+            // }
+        }
+    };
+
+    // return
     return {
         turn,
         switchTurn,
@@ -64,16 +99,14 @@ const gameBoard = (() => {
     const cells = document.querySelectorAll(".cell");
     cells.forEach((cell) => {
         cell.addEventListener("click", () => {
-            if (game.turn == true && cell.textContent=="") {
-            cell.textContent = "X";
-            game.switchTurn();
-            }
-            else if (game.turn == false && cell.textContent=="") {
-            cell.textContent = "O";
-            game.switchTurn();
+            if (game.turn == true && cell.textContent == "") {
+                cell.textContent = "X";
+                game.switchTurn();
+            } else if (game.turn == false && cell.textContent == "") {
+                cell.textContent = "O";
+                game.switchTurn();
             }
         });
-         
     });
     return {
         startBoard,
