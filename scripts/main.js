77a35586a8d3f27aab488020a33a182c9296a4da
Kenny Lee <kenny.lee927@gmail.com>
2022-05-03 01:40:43 -0400

    write basic logic to check for winner

--- scripts/main.js
+++ scripts/main.js
@@ -38,37 +38,41 @@
 const game = (() => {
     // keep track of who's turn it is
     let turn = true;
-    const headertest = document.querySelector("header");
+    const headertest = document.querySelector("h1");
     const switchTurn = function () {
         if (this.turn == true) {
             this.turn = false;
-            headertest.textContent = "Player 1's turn";
+            headertest.textContent = "Player 2's turn";
         } else {
             this.turn = true;
-            headertest.textContent = "Player 2's turn";
+            headertest.textContent = "Player 1's turn";
         }
     };
 
     // after every move, check for a winner
-    const winningCombos = [
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9],
-        [1, 4, 7],
-        [2, 5, 8],
-        [3, 6, 9],
-        [1, 5, 9],
-        [3, 5, 7],
-    ];
+
     // const player1total = array of all of player 1's ticks
     // const player2total = array of all of player 2's ticks
 
     const checkWin = function () {
+        const winningCombos = [
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9],
+            [1, 4, 7],
+            [2, 5, 8],
+            [3, 6, 9],
+            [1, 5, 9],
+            [3, 5, 7],
+        ];
         // for every winning combo,
-        for (let i = 0; i < winningCombos.length(); i++) {
+        for (let i = 0; i < winningCombos.length; i++) {
             // for every cell in the winning combo
                 const winner = winningCombos[i];
-                let checker = (winner, playerTotal) => winner.every(v => winner.includes(v));
+                console.log ("Checking winning numbers "  + winner)
+                let checker = (playerTotal, winner) => winner.every(v => playerTotal.includes(v));
+                console.log("Player score is " + gameBoard.playerTotal.map(Number));
+                console.log(checker(gameBoard.playerTotal.map(Number), winner));
                 // let array1 = [1,2,3],
                 // array2 = [1,2,3,4],
                 // array3 = [1,2];
@@ -86,6 +90,7 @@ const game = (() => {
     return {
         turn,
         switchTurn,
+        checkWin,
     };
 })();
 
@@ -97,19 +102,27 @@ const gameBoard = (() => {
         [7, 8, 9],
     ];
     const cells = document.querySelectorAll(".cell");
+    let playerTotal = [];
+
     cells.forEach((cell) => {
         cell.addEventListener("click", () => {
             if (game.turn == true && cell.textContent == "") {
                 cell.textContent = "X";
+                console.log(cell.id);
+                playerTotal.push(cell.id);
+                console.log(playerTotal);
+                game.checkWin()
                 game.switchTurn();
             } else if (game.turn == false && cell.textContent == "") {
                 cell.textContent = "O";
+                console.log(cell.id);
                 game.switchTurn();
             }
         });
     });
     return {
         startBoard,
+        playerTotal
     };
 })();
 
